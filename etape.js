04af261c9674d843d35866f48264ffcafdadb2d1
Annie Vu <annievu9@gmail.com>
2017-03-21 15:56:59 -0400

    ajout code de base pour chaque etape

--- etape.js
+++ etape.js
@@ -9,22 +9,48 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
+//afficher le index.ejs
 app.get('/',  (req, res) => {
 	res.render("index.ejs");
 
-})
-
-//etape 1
-app.get('/fichier', function (req, res) {
-	fs.readFile( __dirname + "/public/text/" + "collection_provinces.json", 'utf8', function (err, data) {
-		console.log( data );
-		res.end(data);
-	});
 })
 
 var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
 
-   console.log("Exemple l'application écoute sur http://%s", host, port)
+   console.log("L'application écoute sur http://%s", host, port)
+})
+
+//etape 1
+app.get('/fichier', function (req, res) {
+	fs.readFile(__dirname + "/public/text/" + "collection_provinces.json", 'utf8', function (err, data) {
+		res.end(data);
+	});
+})
+
+//etape 2
+app.get('/provinces', function (req, res) {
+	fs.readFileSync(__dirname + "/public/text/" + "collection_provinces.json", "utf8")
+	res.render("index.ejs")
+})
+
+//etape 3
+app.get('/collection', function (req, res) {
+	res.render("index.ejs")
+})
+
+//etape 4
+app.get('/ajouterUn', function (req, res) {
+	res.render("index.ejs")
+})
+
+//etape 5
+app.get('/detruire', function (req, res) {
+	res.render("index.ejs")
+})
+
+//etape 6
+app.get('/ajouterPlusieurs', function (req, res) {
+	res.render("index.ejs")
 })
