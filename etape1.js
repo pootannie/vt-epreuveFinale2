4cde633699c49d66538405a6369cdf910e211b97
Annie Vu <annievu9@gmail.com>
2017-03-21 14:33:27 -0400

    fin etape 1

--- etape1.js
+++ etape1.js
@@ -9,5 +9,17 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
+//acces au json
+app.get('/', function (req, res) {
+	fs.readFile( __dirname + "/public/text/" + "collection_provinces.json", 'utf8', function (err, data) {
+		console.log( data );
+		res.end(data);
+	});
+})
 
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
 
+   console.log("Exemple l'application écoute sur http://%s", host, port)
+})
